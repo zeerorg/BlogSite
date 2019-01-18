ad898cd798996af1bd037d3618da896cb344d746
Rishabh Gupta <r.g.gupta@outlook.com>
2019-01-18 14:13:57 +0530

    Center main page by adding bootstrap columns

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,26 +5,32 @@ import PostList from "./PostList";
 
 const Home = function(props) {
   useEffect(() => {
-    document.title = "Rishabh's Blog"
+    document.title = "Rishabh's Blog";
   }, []);
 
   return (
     <React.Fragment>
-      <div className="my-5">
-        <h1>
-          <Link to="/" className="no-link-style">
-            Rishabh's Blog
-          </Link>
-        </h1>
+      <div className="row">
+        <div className="col-md-2" />
+        <div className="col-md-8">
+          <div className="my-5">
+            <h1>
+              <Link to="/" className="no-link-style">
+                Rishabh's Blog
+              </Link>
+            </h1>
+          </div>
+          <p className="my-5">
+            I'm Rishabh, I blog about software development and some casual
+            stuff.
+            <br />
+            <a href="https://twitter.com/thisIsRGupta">@thisIsRGupta</a> /{" "}
+            <a href="mailto:r.g.gupta@outlook.com">r.g.gupta@outlook.com</a>
+          </p>
+          <PostList />
+          <a href="/rss.xml">RSS Feed</a>
+        </div>
       </div>
-      <p className="my-5">
-        I'm Rishabh, I blog about software development and some casual stuff.
-        <br />
-        <a href="https://twitter.com/thisIsRGupta">@thisIsRGupta</a> /{" "}
-        <a href="mailto:r.g.gupta@outlook.com">r.g.gupta@outlook.com</a>
-      </p>
-      <PostList />
-      <a href="/rss.xml">RSS Feed</a>
     </React.Fragment>
   );
 };
